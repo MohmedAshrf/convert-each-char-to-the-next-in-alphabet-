ecbf1f2fdaa542d292e758ca948aec30aa009755
Mohamed Ashraf <43587732+MohmedAshrf@users.noreply.github.com>
2021-05-22 22:56:52 +0200

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,15 @@
+def convert(string): 
+    result = ""
+    for i in string:
+        i=ord(i)
+        if (i >= 65 and i <= 90) or (i >= 97 and i <= 122): #indicates alphabet
+            i+=1
+            char = chr(i)
+            if char in ('e', 'i', 'o', 'u'):
+                char = char.upper()
+            result += char
+        else:
+            result += i
+    return(result)
+    
+print(convert(input("Enter a string to be manipulated")))
